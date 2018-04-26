767463bbe275f819484d607c76b6364689d50d98
Maxim Stomphorst <maxim@stomphorst.eu>
2018-04-26 20:10:55 +0200

    Init { Project UI setup {

--- SetStanford2017/ViewController.swift
+++ SetStanford2017/ViewController.swift
@@ -9,17 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    @IBAction func selectCard(_ sender: UIButton) {
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        for card in cardButtons.indices {
+            cardButtons[card].backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+        }
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
